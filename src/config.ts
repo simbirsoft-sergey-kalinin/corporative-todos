2e6362f41600f91bed7c6e87774ce277da2e371e
kalinin <sergey.kalinin@simbirsoft.com>
2020-12-04 19:08:59 +0300

    empty

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_URI = 'https://corporative-todos.com';
-export const STATISTICS_URI = 'https://statistics.com';
-export const checkAuthorizationUrl = '/check-auth';
-export const logoutUrl = '/logout';
-export const credentials = 'same-origin';
